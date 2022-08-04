cccb35dd4df96893452472be392303ed84911ff5
Malika Muslieva <malika.muslieva@accenture.com>
2022-08-04 11:02:00 +0200

    Applitools solution after refactoring

--- ComparingImages/src/main/java/tasks/firstResearchTask/pages/HomePage.java
+++ ComparingImages/src/main/java/tasks/firstResearchTask/pages/HomePage.java
@@ -1,5 +1,8 @@
 package tasks.firstResearchTask.pages;
 
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.images.Eyes;
+import com.applitools.eyes.images.Target;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import ru.yandex.qatools.ashot.comparison.ImageDiff;
@@ -12,14 +15,19 @@ import java.io.IOException;
 
 import static tasks.firstResearchTask.DriverFactory.getChromeDriver;
 
-// validating images using AShot
+// validating images using AShot, Applitools and Sikuli (Sikuli ist noch offen)
 public class HomePage {
+
+
+    private String APIKeyForApplitools = "SRzr54R78ihRyHhZI99sV70kfOdk7wtddTdCYqVU3Aec110";
+    private Eyes eyes = new Eyes();
 
 
     private WebDriver driver = getChromeDriver();
     private JavascriptExecutor js = (JavascriptExecutor) driver;
 
     private By picture = By.xpath("//body[1]/div[2]/section[1]/div[1]/ul[1]/li[5]/div[1]/a[1]/picture[1]/img[1]");
+
 
     public File magazinImageFile() {
         // scroll the image I want to capture a Screenshot of  into view
@@ -28,7 +36,6 @@ public class HomePage {
 
         // wrap it with an if-else statement - take a screenshot and save as a file
         File srcImageFile = pictureToValidate.getScreenshotAs(OutputType.FILE);
-        // create new Target location
 
         return srcImageFile;
 
@@ -48,6 +55,8 @@ public class HomePage {
         // read the expected image and save in the BufferedImage object
         BufferedImage expectedImage = ImageIO.read(new File(".\\screenshots\\magazin.png"));
 
+        // to make the test replace the image magazinSecondScreenshot.png
+
         // now take screenshot of the current Image
         File srcImageFile = magazinImageFile();
         BufferedImage actualImage = ImageIO.read(srcImageFile);
@@ -64,7 +73,31 @@ public class HomePage {
         }
     }
 
-    public Boolean validatingCapturedScreenshotUsingApplitools(){
+    public Boolean validatingCapturedScreenshotUsingApplitools() {
+
+        eyes.setHostOS("Windows 11");
+        //eyes.setHostApp("HomePageTest");
+        eyes.setApiKey(APIKeyForApplitools);
+
+        try {
+            // the name of the class my test is in, the name of the test method, and size I want the screenshot to have
+            //eyes.open("HomePageTest", "screenshotImageAndValidateItUsingApplitools", new RectangleSize(800, 600));
+
+            eyes.open("HomePage", "validatingCapturedScreenshotUsingApplitools", new RectangleSize(800, 600));
+            BufferedImage expectedImage = ImageIO.read(new File(".\\screenshots\\magazin.png"));
+
+            // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+            // no scrollingIntoView required - to make the test replace the image magazinSecondScreenshot.png
+            eyes.check("Image buffer", Target.image(expectedImage));
+
+            eyes.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            // If the test was aborted before eyes.close was called, ends the test as aborted
+            eyes.abortIfNotClosed();
+        }
 
         return true;
     }
--- ComparingImages/src/test/java/tasks/firstResearchTask/HomePageTest.java
+++ ComparingImages/src/test/java/tasks/firstResearchTask/HomePageTest.java
@@ -1,30 +1,21 @@
 package tasks.firstResearchTask;
 
-import com.applitools.eyes.RectangleSize;
-import com.applitools.eyes.images.Eyes;
-import com.applitools.eyes.images.Target;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 import tasks.firstResearchTask.pages.HomePage;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class HomePageTest extends BaseTestClass {
 
     private HomePage homePage = new HomePage();
-    private String APIKeyForApplitools = "SRzr54R78ihRyHhZI99sV70kfOdk7wtddTdCYqVU3Aec110";
 
     @Test
     public void screenshotImageAndValidateItUsingAShot() {
         try {
             Assert.assertNotNull(homePage.captureScreenshotOfImage());
             Assert.assertTrue(homePage.validatingCapturedScreenshotUsingAShot());
-        }catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
 /*
@@ -35,36 +26,13 @@ public class HomePageTest extends BaseTestClass {
 
     @Test
     public void screenshotImageAndValidateItUsingApplitools() {
-        // refactor - move to setUp to BeforeTest Method just fo this test
-        // move required code to the HomePage Class, just like in the case above
-        Eyes eyes = new Eyes();
-
-        eyes.setHostOS("Windows 11");
-        eyes.setHostApp("HomePage");
-        eyes.setApiKey(APIKeyForApplitools);
 
         try {
             Assert.assertNotNull(homePage.captureScreenshotOfImage());
-            // the name of the class my test is in, the name of the test method, and size I want the screenshot to have
-            eyes.open("HomePageTest", "screenshotImageAndValidateItUsingApplitools", new RectangleSize(800, 600));
+            Assert.assertTrue(homePage.validatingCapturedScreenshotUsingApplitools());
 
-            // get the saved image-file
-            BufferedImage expectedImage = ImageIO.read(new File(".\\screenshots\\magazin.png"));
-
-            // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
-            // https://applitools.com/docs/api-ref/sdk-api/selenium/java/eyes/#check-method
-
-            // no scrollingIntoView required :)
-            eyes.check("Image buffer", Target.image(expectedImage));
-
-            // end the visual UI testing
-            eyes.close();
         } catch (IOException ex) {
             ex.printStackTrace();
-        } finally {
-            // If the test was aborted before eyes.close was called, ends the test as aborted
-            eyes.abortIfNotClosed();
         }
-
     }
 }
